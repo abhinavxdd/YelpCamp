38a7d4ed259cd2b99bb705f6e211e1d1a99a73a3
abhinavxdd <abhinavnerd@gmail.com>
2025-04-03 19:08:32 +0530

    add error changes

--- seeds/index.js
+++ seeds/index.js
@@ -24,9 +24,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/1920/1080?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et hic saepe est, ex ad fugiat delectus minima, doloribus maiores, obcaecati architecto quasi quidem? Perferendis, minus fugiat id voluptas dolor amet!',
+            price: price
         })
         await camp.save();
     }
