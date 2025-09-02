66820f66021458d41f21e78587956a75bb42ddb6
abhinavxdd <abhinavnerd@gmail.com>
2025-09-02 20:02:21 +0530

    fix seeds

--- seeds/index.js
+++ seeds/index.js
@@ -29,9 +29,19 @@ const seedDB = async () => {
             author: '68b4994ed15b85965ba4097a',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: `https://picsum.photos/400/300?random=${Math.random()}`,
+            // image: `https://picsum.photos/400/300?random=${Math.random()}`,
             description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et hic saepe est, ex ad fugiat delectus minima, doloribus maiores, obcaecati architecto quasi quidem? Perferendis, minus fugiat id voluptas dolor amet!',
-            price: price
+            price: price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dvyljgvg5/image/upload/v1756822970/YelpCamp/zn5oh39f1dsn6xgowqjw.webp',
+                    filename: 'YelpCamp/zn5oh39f1dsn6xgowqjw'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dvyljgvg5/image/upload/v1756822971/YelpCamp/egvzvdoycilgaimn3rwx.jpg',
+                    filename: 'YelpCamp/egvzvdoycilgaimn3rwx'
+                }
+            ]
         })
         await camp.save();
     }
