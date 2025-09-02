f9fffb500ae3ddf899d73cece55874d9563dd8a5
abhinavxdd <abhinavnerd@gmail.com>
2025-09-03 03:01:43 +0530

    add final image upload features

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,7 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary');
+const maptilerClient = require("@maptiler/client");
+maptilerClient.config.apiKey = process.env.MAPTILER_API_KEY;
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -10,7 +13,9 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res, next) => {
+    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
     const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.features[0].geometry;
     if (req.files && req.files.length > 0) {
         campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     }
@@ -46,17 +51,42 @@ module.exports.renderEditForm = async (req, res) => {
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+
+    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
+    campground.geometry = geoData.features[0].geometry;
+    
+    // Add new images if any were uploaded
     if (req.files && req.files.length > 0) {
         const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
         campground.images.push(...imgs);
-        await campground.save();
     }
+    
+    // Delete selected images
+    if (req.body.deleteImages) {
+        // Delete from cloudinary
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        // Remove from database
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+    }
+    
+    await campground.save();
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 };
 
 module.exports.deleteCampground = async (req, res) => {
     const { id } = req.params;
+    const campground = await Campground.findById(id);
+    
+    // Delete all images from cloudinary
+    if (campground.images) {
+        for (let image of campground.images) {
+            await cloudinary.uploader.destroy(image.filename);
+        }
+    }
+    
     await Campground.findByIdAndDelete(id);
     req.flash('warning', 'Successfully deleted campground');
     res.redirect('/campgrounds');
